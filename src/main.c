98b2a8014736596562f4d378baabe4b6346db878
semch kechad <semch.kechad@ge1.iut-cachan.u-psud.fr>
2018-11-13 09:46:03 +0100

    Exo 2 corrigé

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
 
 int main(void)
 {
+    unsigned long long int S1 = 1;
+    unsigned char n = 2;
+
+    while(n<50)
+    {
+        S1 = S1 * n;
+        n = n + 1;
+    }
+
     return 0;
 }
