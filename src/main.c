ab6792e7e334520fd4c5478cfa83853d56dbd795
semch kechad <semch.kechad@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:30:03 +0100

    exo56_p1

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
+
+unsigned char bin[10] = {200 , 150 , 129 , 114 , 118, 120 , 145 , 149 , 127 , 114};
+int seuil = 155 , i=0;
 
 int main(void)
 {
+for (i=0 ; i<10 ;i++)
+{
+
+if (bin[i] >= seuil )
+{
+bin[i] =1;
+}
+
+else 
+{
+bin[i] =0;
+}	
+}	
     return 0;
 }
