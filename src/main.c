482a12f4852ca7c1ecca85e839d1592b2ba660ad
skechad <semch.kechad@u-psud.fr>
2018-11-27 22:29:27 +0100

    exo56_partie1

--- src/main.c
+++ src/main.c
@@ -2,8 +2,30 @@
 //
 
 #include "my_header.h"
+#include "stdio.h"
+#include "TP_lib_suite.h"
 
 int main(void)
 {
+unsigned char loto[6]={44,32,24,12,25,8};
+unsigned char loto2[6]={50,50,50,50,50,50};
+unsigned char i=0,h=0,Gain=0;
+
+
+
+
+for (i=0;i<6; i++)
+{
+loto2[i]=tirerNumero();
+
+for(h=0;h<6;h++)
+{
+if(loto[h]==loto2[i])
+Gain++;
+}
+
+}
+	
+	
     return 0;
 }
