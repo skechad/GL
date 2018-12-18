fef51d4f84ed1596da05d96db7c12889b914c0f8
semch kechad <semch.kechad@ge1.iut-cachan.u-psud.fr>
2018-12-18 11:22:12 +0100

    mainres

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <iostream>
+#include <my_header.hpp>
+#include <cmath>
+#include <cstdint>
 
-#include "my_header.h"
-
-int main(void)
+int main()
 {
+    long int anneau1, anneau2, anneau3, val_res;
+    int16_t R;
+
+    anneau1 = saisir_anneau(0,9,1);
+
+    anneau2 = saisir_anneau(0,9,2);
+
+    anneau3 = saisir_anneau(0,6,3);
+
+
+    // if (c > 7)
+    //     R = -1;
+    // else
+    //     R = (10 * a + b) * (uint16_t)pow(10,c);
+
+
     return 0;
 }
